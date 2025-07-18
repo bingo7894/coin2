cb863564c57a3bf4505ffbccbcdba23b7d234586
bingo7894 <pokpong.padjunreed@gmail.com>
2025-07-18 20:31:55 +0700

    Add start script for Render deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
@@ -11,45 +12,5 @@ export default defineNuxtConfig({
   runtimeConfig: {
     githubId: "",
     githubSecret: "",
-  },
-
-  nitro: {
-    handlers: [
-      {
-        route: "/api/auth/github",
-        method: "get",
-        handler: "~/server/api/auth/github.get.ts",
-      },
-      {
-        route: "/api/auth/register",
-        method: "post",
-        handler: "~/server/api/auth/register.post.ts",
-      },
-      {
-        route: "/api/account/stats",
-        method: "get",
-        handler: "~/server/api/account/stats.get.ts",
-      },
-      {
-        route: "/api/history/:id",
-        method: "get",
-        handler: "~/server/api/history/[id].get.ts",
-      },
-      {
-        route: "/api/history",
-        method: "get",
-        handler: "~/server/api/history/index.get.ts",
-      },
-      {
-        route: "/api/history",
-        method: "post",
-        handler: "~/server/api/history/index.post.ts",
-      },
-      {
-        route: "/api/report",
-        method: "post",
-        handler: "~/server/api/report/index.post.ts",
-      },
-    ],
   },
 });
